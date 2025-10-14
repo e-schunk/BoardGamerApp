d8c6eba99be85e634c3c5728f2c3c166fb2842af
Eduard Schunk <eduard.schunk85@gmail.com>
2025-10-14 21:39:19 +0200

    APK Release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
