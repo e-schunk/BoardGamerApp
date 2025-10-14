00aac8eea7854265de5e30e4b1b960eff0d63368
Eduard Schunk <eduard.schunk85@gmail.com>
2025-10-15 00:24:31 +0200

    build: Fix Google services plugin id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.google.gms.google.services)
     id("com.google.firebase.crashlytics")
     id("com.github.triplet.play") version "3.12.1"
+    id("com.google.gms.google-services")
 }
 
 android {
