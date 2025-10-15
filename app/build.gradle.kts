7b7c4cfa7b83e85d57b012d3b7c7d5900bec59b8
Eduard Schunk <eduard.schunk85@gmail.com>
2025-10-16 00:16:28 +0200

    Entfernung der Signing-Verifizierung

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
+
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
